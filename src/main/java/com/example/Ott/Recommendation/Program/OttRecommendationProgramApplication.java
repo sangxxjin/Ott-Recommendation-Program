f1a1bc7ea018e611194e32a1c60259c80477ba10
sangxxjin <sj_0368@naver.com>
2024-03-23 18:47:55 +0900

    Initial commit

---
+++ src/main/java/com/example/Ott/Recommendation/Program/OttRecommendationProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ott.Recommendation.Program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttRecommendationProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttRecommendationProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ott/Recommendation/Program/OttRecommendationProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ott.Recommendation.Program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttRecommendationProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
