13a5881c29d582c9f409be8c209ff641f83f6b00
sangxxjin <sj_0368@naver.com>
2024-04-14 16:23:15 +0900

    feat: 로그인 회원가입 구현 및 토큰 생성
    본문 :

--- src/main/java/com/example/Ott/Recommendation/Program/service/MemberService.java
+++ src/main/java/com/example/Ott/Recommendation/Program/service/MemberService.java
@@ -1,11 +1,18 @@
 package com.example.Ott.Recommendation.Program.service;
 
-import com.example.Ott.Recommendation.Program.Dto.SignRequest;
+import com.example.Ott.Recommendation.Program.dto.SignRequest;
+import com.example.Ott.Recommendation.Program.dto.SignResponse;
+import com.example.Ott.Recommendation.Program.dto.TokenDto;
 import com.example.Ott.Recommendation.Program.domain.Member;
+import com.example.Ott.Recommendation.Program.domain.Token;
 import com.example.Ott.Recommendation.Program.repository.MemberRepository;
+import com.example.Ott.Recommendation.Program.repository.TokenRepository;
+import com.example.Ott.Recommendation.Program.security.JwtProvider;
 import jakarta.transaction.Transactional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +21,9 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
   private final MemberRepository memberRepository;
-
+  private final PasswordEncoder passwordEncoder;
+  private final TokenRepository tokenRepository;
+  private final JwtProvider jwtProvider;
 
   public boolean join(SignRequest request) {
 
@@ -29,7 +38,7 @@ public class MemberService {
       Member member = Member.builder()
           .email(request.getEmail())
           .nickname(request.getNickname())
-          .password(request.getPassword())
+          .password(passwordEncoder.encode(request.getPassword()))
           .phone(request.getPhone())
           .build();
       memberRepository.save(member);
@@ -38,5 +47,40 @@ public class MemberService {
       throw new IllegalArgumentException("잘못된 요청입니다.");
     }
     return true;
+  }
+  public SignResponse login(SignRequest request) {
+    if (request.getEmail().isEmpty() || request.getPassword().isEmpty()) {
+      throw new IllegalArgumentException("빈칸 없이 입력해주세요.");
+    }
+
+    Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(() ->
+        new IllegalArgumentException("존재하지 않는 이메일입니다."));
+
+    if (!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+      throw new IllegalArgumentException("로그인 정보가 올바르지 않습니다.");
+    }
+
+    member.setRefreshToken(createRefreshToken(member));
+
+    return SignResponse.builder()
+        .email(member.getEmail())
+        .nickname(member.getNickname())
+        .phone(member.getPhone())
+        .token(TokenDto.builder()
+            .accessToken(jwtProvider.createToken(member.getEmail()))
+            .refreshToken(member.getRefreshToken())
+            .build())
+        .build();
+  }
+
+  public String createRefreshToken(Member member) {
+    Token token = tokenRepository.save(
+        Token.builder()
+            .email(member.getEmail())
+            .refresh_token(UUID.randomUUID().toString())
+            .expiration(300)
+            .build()
+    );
+    return token.getRefresh_token();
   }
 }
